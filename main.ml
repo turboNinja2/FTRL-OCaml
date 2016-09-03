2138b1d7968e3f195f9892c1a3de3081ead01237
turboNinja2 <julien.revelle@gmail.com>
2016-09-03 15:14:14 +0200

    Main works with side effects on "weights"

--- main.ml
+++ main.ml
@@ -18,28 +18,28 @@ let rec update indices weights p y alpha =  match indices with
 let n = pow 2 20 ;;
 let weights = Array.make n 0. ;;
 let dict_stream = dict_reader "train_small.csv" ;;
-let updater indices weights p y = update indices weights p y 0.01 ;;
+let updater indices p y = update indices weights p y 0.01 ;;
 let refresh_loss = 1000000 ;;
 
 (* training *)
 
-let train weights updater dict_stream = 
-	let rec aux weights updater dict_stream t loss n = match (try Some( Stream.next dict_stream) 
+let train updater dict_stream = 
+	let rec aux updater dict_stream t loss n = match (try Some(Stream.next dict_stream) 
 								  with _ -> None) with
 	| Some dict -> Hashtbl.remove dict "id"; 
 			  let y = float_of_string (Hashtbl.find dict "click") in
 			  Hashtbl.remove dict "click";
 			  let indices = get_indices dict n in
 			  let p = predict indices weights in
-			  updater indices weights p y;
+			  updater indices p y;
 			  
 			  if ((t mod refresh_loss) == 0) && t > 0 then begin 
 				  print_float (loss /. float_of_int(t)); 
 				  print_endline " "; 
 			  end;
 			  
-			  aux weights updater dict_stream (t + 1) (loss +. (log_loss p y)) n
+			  aux updater dict_stream (t + 1) (loss +. (log_loss p y)) n
 			  
-	| None -> () in aux weights updater dict_stream 0 0. (Array.length weights);;	
+	| None -> () in aux updater dict_stream 0 0. (Array.length weights);;	
 
-train weights updater dict_stream;
+train updater dict_stream;;
