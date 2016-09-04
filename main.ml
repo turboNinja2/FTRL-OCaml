36a6c04f6530f72fa8e18fdae638448bb83034ab
turboNinja2 <julien.revelle@gmail.com>
2016-09-04 22:10:03 +0200

    Useless refactoring

--- main.ml
+++ main.ml
@@ -10,8 +10,8 @@ let train_dict_stream = dict_reader "train_small.csv" ;;
 
 let n = pow 2 20 ;;
 let weights = Array.make n 0. ;;
-let refresh_loss = 10000 ;;
-let eta = 0.01
+let refresh_loss = 1000000 ;;
+let alpha = 0.01
 
 (* feature engineering *)
 
@@ -21,12 +21,12 @@ let feature_engineer dict =  _get_indices dict n ;;
 
 (* logistic regression *)
 
-let rec _update indices weights p y alpha =  match indices with
+let rec _update indices weights step =  match indices with
 	| [] -> ()
-	| h::tail -> weights.(h) <- (weights.(h) -. (p -. y) *. alpha) ; _update tail weights p y alpha ;;
+	| h::tail -> weights.(h) <- (weights.(h) -. step) ; _update tail weights step ;;
 
 let predict indices = sigmoid (dot_product indices weights) ;;
 
-let update indices p y = _update indices weights p y eta ;;
+let update indices p y = _update indices weights ((p -. y) *. alpha) ;;
 
 train update train_dict_stream feature_engineer predict log_loss refresh_loss "click";;
